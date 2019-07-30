b2a8bcd579eb880ecdf744e2b623a1ab2b6ceb36
Section1 <Section1@WISSEN>
2019-07-30 14:55:47 +0300

    builder ve global filtre tanımlandı

---
+++ SmallCRM.Data/Builders/CustomerBuilder.cs
@@ -0,0 +1,19 @@
+﻿using SmallCRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Data.Builders
+{
+    public class CustomerBuilder
+    {
+        public CustomerBuilder(EntityTypeConfiguration<Customer> builder)
+        {       
+            builder.Property(b => b.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(b => b.LastName).HasMaxLength(50).IsRequired();
+        }
+    }
+}
---
+++ SmallCRM.Data/EntityFrameworkExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Dynamic;
+
+namespace SmallCRM.Data
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static IQueryable<T> Where<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate)
+        {
+            source = source.Where("IsDeleted == false"); // From System.linq.Dynamic Library
+            source = Queryable.Where<T>(source, predicate);
+            return source;
+        }
+    }
+}
