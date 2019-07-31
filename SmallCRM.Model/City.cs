020a424159ac3eee771b512f94b8837c2ff072bd
Section1 <Section1@WISSEN>
2019-07-31 14:15:55 +0300

    entityler oluşturuluyor

---
+++ SmallCRM.Model/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Model
+{
+    public class City:BaseEntity
+    {
+        public string Name { get; set; }
+        public Guid CountryId { get; set; }
+        public virtual Country Country { get; set; }
+
+        public virtual ICollection<Region> Regions { get; set; }
+
+        public virtual ICollection<Lead> Leads { get; set; }
+
+        public virtual ICollection<Contact> PostalContacts { get; set; }
+        public virtual ICollection<Contact> OtherContacts { get; set; }
+    }
+}
