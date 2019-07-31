88df97cdb50ce5e192b674ce10603799bad08833
yalcinpkr <yalcin_peker@hotmail.com>
2019-07-31 15:04:40 +0300

    Feed Changes

---
+++ SmallCRM.Model/Feed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Model
+{
+   public class Feed: BaseEntity
+    {
+        public string Message { get; set; }
+        public string FilePath { get; set; }
+        public string Email { get; set; }
+        public bool Status { get; set; }
+        public string Comment { get; set; }
+    }
+}
