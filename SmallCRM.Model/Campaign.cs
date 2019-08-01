5f8e07d5536081c6d62f89cc40e45a1d7de80b34
burcu saka <koral.burcu@hotmail.com>
2019-08-01 10:54:50 +0300

    merge

---
+++ SmallCRM.Model/Campaign.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Model
+{
+    public class Campaign:BaseEntity
+    {
+        public string Owner { get; set; }
+        public OpportunityType OpportunityType { get; set; }
+        public CampaignStatus? CampaignStatus { get; set; }
+        public string Name { get; set; }
+        public DateTime? StartDate { get; set; }
+        public decimal? ExpectedEndorsement { get; set; } //Beklenen ciro
+        public decimal? Cost { get; set; } //Maliyet(Gerçekleşen masraf)
+        public string SendPhoneNumbers { get; set; } //Gönderilen Numaralar
+        public DateTime? EndDate { get; set; }
+        public decimal? PlannedCost { get; set; } //Bütçelenmiş masraf
+        public string ExpectedResponse { get; set; } //Beklenen tepki
+        public string Description { get; set; }
+    }
+}
---
+++ SmallCRM.Model/CampaignStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Model
+{
+    public enum CampaignStatus
+    {
+        None=0,
+        Planning = 1,
+        Active = 2,
+        NonActive = 3,
+        Completed = 4
+    }
+   
+}
