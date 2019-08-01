1ac574627ee6291c3b309b588ddca318b830f1a5
tugceguclu <tugceegucluu@gmail.com>
2019-08-01 14:53:39 +0300

    Project&CampaignSource Bilders

---
+++ SmallCRM.Data/Builders/CampaignSourceBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Data.Builders
+{
+    public class CampaignSourceBuilder
+    {
+        public CampaignSourceBuilder(EntityTypeConfiguration<CampaignSourceBuilder> builder)
+        {
+            builder.Property(b => b.Name).HasMaxLength(100).IsRequired();
+
+        }
+    }
+}
---
+++ SmallCRM.Data/Builders/ProjectBuilder.cs
@@ -0,0 +1,19 @@
+﻿using SmallCRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Data.Builders
+{
+     public class ProjectBuilder
+    {
+        public ProjectBuilder(EntityTypeConfiguration<Project> builder)
+        {
+            builder.Property(b => b.Name).HasMaxLength(100).IsRequired();
+            
+        }
+    }
+}
