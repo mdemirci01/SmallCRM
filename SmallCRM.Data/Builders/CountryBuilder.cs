a478e2fb00cf204070470f02520c9db4ebcc84b8
BusraOzdemir <busra.ozdemir@ceng.ktu.edu.tr>
2019-08-02 09:17:35 +0300

    merrge

---
+++ SmallCRM.Data/Builders/CountryBuilder.cs
@@ -0,0 +1,18 @@
+﻿using SmallCRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SmallCRM.Data.Builders
+{
+    class CountryBuilder
+    {
+        public CountryBuilder(EntityTypeConfiguration<Country> builder)
+        {
+            builder.Property(b => b.Name).HasMaxLength(50).IsRequired();
+        }
+    }
+}
