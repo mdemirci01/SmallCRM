8a186610ba02eecb92e818028d35eca0a2b709aa
burcu saka <koral.burcu@hotmail.com>
2019-08-02 09:36:33 +0300

    FeedBuilder ve TimeSpendingBuilder

---
+++ SmallCRM.Data/Builders/FeedBuilder.cs
@@ -0,0 +1,21 @@
+﻿using SmallCRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Data.Builders
+{
+    public class FeedBuilder
+    {
+        public FeedBuilder(EntityTypeConfiguration<Feed> builder)
+        {
+            builder.Property(b => b.Message).HasMaxLength(200).IsRequired();
+            builder.HasOptional(a => a.Document).WithMany(b => b.Feeds).HasForeignKey(a => a.DocumentId);
+            builder.Property(b => b.TargetUser).IsRequired();
+
+        }
+    }
+}
