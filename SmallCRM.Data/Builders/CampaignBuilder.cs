514be88e61771906f80d98a56a30aba11e722eed
BusraOzdemir <busra.ozdemir@ceng.ktu.edu.tr>
2019-08-02 09:07:27 +0300

    Campaign and Document Builder

---
+++ SmallCRM.Data/Builders/CampaignBuilder.cs
@@ -0,0 +1,21 @@
+﻿using SmallCRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Data.Builders
+{
+    public class CampaignBuilder
+    {
+        public CampaignBuilder(EntityTypeConfiguration<Campaign> builder)
+        {
+            builder.Property(b => b.Owner).HasMaxLength(50);
+            builder.Property(b => b.Name).HasMaxLength(50).IsRequired();            
+            builder.Property(b => b.ExpectedResponse).HasMaxLength(30);
+            builder.Property(b => b.Description).HasMaxLength(200);
+        }
+    }
+}
