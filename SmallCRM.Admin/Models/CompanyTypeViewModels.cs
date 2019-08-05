1ce956f578349268e52ed8a40d3b023fd9eb794b
Section1 <Section1@WISSEN>
2019-08-05 10:59:01 +0300

    CompanyTypeViewModel Eklendi

---
+++ SmallCRM.Admin/Models/CompanyTypeViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmallCRM.Admin.Models
+{
+    public class CompanyTypeViewModels
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [Display(Name="Firma Tipi")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
