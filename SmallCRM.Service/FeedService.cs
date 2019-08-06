2cc3740fe4bc6d12b4041b38e1bf146bc8492e25
burcu saka <koral.burcu@hotmail.com>
2019-08-06 11:24:24 +0300

    güncelleme

---
+++ SmallCRM.Service/FeedService.cs
@@ -0,0 +1,68 @@
+﻿using SmallCRM.Data;
+using SmallCRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Service
+{
+    public class FeedService:IFeedService
+    {
+        private readonly IRepository<Feed> feedRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public FeedService(IRepository<Feed> feedRepository, IUnitOfWork unitOfWork)
+        {
+            this.feedRepository = feedRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public bool Any(Guid id)
+        {
+            return feedRepository.Any(x => x.Id == id);
+        }
+         
+
+        public void Delete(Guid id)
+        {
+        var feed = feedRepository.Get(id);
+        if (feed != null)
+        {
+            feedRepository.Delete(feed);
+            unitOfWork.SaveChanges();
+        }
+        }
+
+        public Feed Get(Guid id)
+        {
+            return feedRepository.Get(id);
+        }
+
+        public IEnumerable<Feed> GetAll()
+        {
+            return feedRepository.GetAll();
+        }
+
+        public void Insert(Feed feed)
+        {
+            feedRepository.Insert(feed);
+            unitOfWork.SaveChanges();
+        }
+
+        public void Update(Feed feed)
+        {
+            feedRepository.Update(feed);
+            unitOfWork.SaveChanges();
+        }
+    }
+    public interface IFeedService
+    {
+        IEnumerable<Feed> GetAll();
+        Feed Get(Guid id);
+        void Insert(Feed feed);
+        void Update(Feed feed);
+        void Delete(Guid id);
+        bool Any(Guid id);
+    }
+}
---
+++ SmallCRM.Service/TimeSpendingService.cs
@@ -0,0 +1,68 @@
+﻿using SmallCRM.Data;
+using SmallCRM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallCRM.Service
+{
+    public class TimeSpendingService:ITimeSpendingService
+    {
+        private readonly IRepository<TimeSpending> timeSpendingRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public TimeSpendingService(IRepository<TimeSpending> timeSpendingRepository, IUnitOfWork unitOfWork)
+        {
+            this.timeSpendingRepository = timeSpendingRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public bool Any(Guid id)
+        {
+            return timeSpendingRepository.Any(x => x.Id == id);
+        }
+
+        public void Delete(Guid id)
+        {
+            var timeSpending = timeSpendingRepository.Get(id);
+            if (timeSpending != null)
+            {
+                timeSpendingRepository.Delete(timeSpending);
+                unitOfWork.SaveChanges();
+            }
+        }
+
+        public TimeSpending Get(Guid id)
+        {
+            return timeSpendingRepository.Get(id);
+        }
+
+        public IEnumerable<TimeSpending> GetAll()
+        {
+            return timeSpendingRepository.GetAll();
+        }
+
+        public void Insert(TimeSpending timeSpending)
+        {
+            timeSpendingRepository.Insert(timeSpending);
+            unitOfWork.SaveChanges();
+        }
+
+        public void Update(TimeSpending timeSpending)
+        {
+            timeSpendingRepository.Update(timeSpending);
+            unitOfWork.SaveChanges();
+        }
+    }
+
+    public interface ITimeSpendingService
+    {
+        IEnumerable<TimeSpending> GetAll();
+        TimeSpending Get(Guid id);
+        void Insert(TimeSpending timeSpending);
+        void Update(TimeSpending timeSpending);
+        void Delete(Guid id);
+        bool Any(Guid id);
+    }
+}
