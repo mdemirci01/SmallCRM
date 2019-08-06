64ded1688f117eb48062d63168fb99770569df1b
Section1 <Section1@WISSEN>
2019-08-06 10:49:26 +0300

    automapper konfigürasyonu tamamlandı

--- SmallCRM.Admin/AutoMapperConfig.cs
+++ SmallCRM.Admin/AutoMapperConfig.cs
@@ -15,23 +15,99 @@ namespace SmallCRM.Admin
         {
             var cfg = new MapperConfigurationExpression();
             cfg.CreateMap<Activity, ActivityViewModel>().ForMember(
-        dest => dest.CampaignOwner,
-        opt => opt.MapFrom(src => src.Campaign.Owner)).ForMember(
-        dest => dest.CompanyOwner,
-        opt => opt.MapFrom(src => src.Company.Owner)).ForMember(
-        dest => dest.ContactOwner,
-        opt => opt.MapFrom(src => src.Contact.Owner)).ForMember(
-        dest => dest.OpportunityOwner,
-        opt => opt.MapFrom(src => src.Opportunity.Owner)).ReverseMap();
+            dest => dest.CampaignOwner,
+            opt => opt.MapFrom(src => src.Campaign.Owner)).ForMember(
+            dest => dest.CompanyOwner,
+            opt => opt.MapFrom(src => src.Company.Owner)).ForMember(
+            dest => dest.ContactOwner,
+            opt => opt.MapFrom(src => src.Contact.Owner)).ForMember(
+            dest => dest.OpportunityOwner,
+            opt => opt.MapFrom(src => src.Opportunity.Owner)).ReverseMap();
             cfg.CreateMap<Feed, FeedViewModel>().ForMember(
-       dest => dest.DocumentName,
-       opt => opt.MapFrom(src => src.Document.Name)).ReverseMap();
+           dest => dest.DocumentName,
+           opt => opt.MapFrom(src => src.Document.Name)).ReverseMap();
             cfg.CreateMap<TimeSpending, TimeSpendingViewModel>().ForMember(
-       dest => dest.ProjectName,
-       opt => opt.MapFrom(src => src.Project.Name)).ForMember(
-       dest => dest.WorkItemName,
-       opt => opt.MapFrom(src => src.WorkItem.Name)).ReverseMap();
-
+            dest => dest.ProjectName,
+            opt => opt.MapFrom(src => src.Project.Name)).ForMember(
+            dest => dest.WorkItemName,
+            opt => opt.MapFrom(src => src.WorkItem.Name)).ReverseMap();
+            cfg.CreateMap<Campaign, CampaignViewModel>().ReverseMap();
+            cfg.CreateMap<CampaignSource, CampaignSourceViewModel>().ReverseMap();
+            cfg.CreateMap<City, CityViewModel>().ForMember(
+            dest => dest.CountryName,
+            opt => opt.MapFrom(src => src.Country.Name)).ReverseMap();
+            cfg.CreateMap<Company, CompanyViewModel>().ForMember(
+            dest => dest.CompanyTypeName,
+            opt => opt.MapFrom(src => src.CompanyType.Name)).ForMember(
+            dest => dest.DeliveryCityName,
+            opt => opt.MapFrom(src => src.DeliveryCity.Name)).ForMember(
+            dest => dest.DeliveryCountryName,
+            opt => opt.MapFrom(src => src.DeliveryCountry.Name)).ForMember(
+            dest => dest.DeliveryRegionName,
+            opt => opt.MapFrom(src => src.DeliveryRegion.Name)).ForMember(
+            dest => dest.InvoiceCityName,
+            opt => opt.MapFrom(src => src.InvoiceCity.Name)).ForMember(
+            dest => dest.InvoiceCountryName,
+            opt => opt.MapFrom(src => src.InvoiceCountry.Name)).ForMember(
+            dest => dest.InvoiceRegionName,
+            opt => opt.MapFrom(src => src.InvoiceRegion.Name)).ForMember(
+            dest => dest.MainCompanyOwner,
+            opt => opt.MapFrom(src => src.MainCompany.Owner)).ForMember(
+            dest => dest.SectorName,
+            opt => opt.MapFrom(src => src.Sector.Name)).ReverseMap();
+            cfg.CreateMap<CompanyType, CompanyTypeViewModel>().ReverseMap();
+            cfg.CreateMap<Contact, ContactViewModel>().ForMember(
+            dest => dest.CityName,
+            opt => opt.MapFrom(src => src.City.Name)).ForMember(
+            dest => dest.CountryName,
+            opt => opt.MapFrom(src => src.Country.Name)).ForMember(
+            dest => dest.LeadSourceName,
+            opt => opt.MapFrom(src => src.LeadSource.Name)).ForMember(
+            dest => dest.OtherCityName,
+            opt => opt.MapFrom(src => src.OtherCity.Name)).ForMember(
+            dest => dest.OtherCountryName,
+            opt => opt.MapFrom(src => src.OtherCountry.Name)).ForMember(
+            dest => dest.OtherRegionName,
+            opt => opt.MapFrom(src => src.OtherRegion.Name)).ForMember(
+            dest => dest.RegionName,
+            opt => opt.MapFrom(src => src.Region.Name)).ForMember(
+            dest => dest.ReportsToContactOwner,
+            opt => opt.MapFrom(src => src.ReportsToContact.Owner)).ReverseMap();
+            cfg.CreateMap<Lead, LeadViewModel>().ForMember(
+            dest => dest.RegionName,
+            opt => opt.MapFrom(src => src.Region.Name)).ForMember(
+            dest => dest.CityName,
+            opt => opt.MapFrom(src => src.City.Name)).ForMember(
+            dest => dest.CountryName,
+            opt => opt.MapFrom(src => src.Country.Name)).ForMember(
+            dest => dest.LeadSourceName,
+            opt => opt.MapFrom(src => src.LeadSource.Name)).ForMember(
+            dest => dest.LeadStatusName,
+            opt => opt.MapFrom(src => src.LeadStatus.Name)).ForMember(
+            dest => dest.SectorName,
+            opt => opt.MapFrom(src => src.Sector.Name)).ReverseMap();
+            cfg.CreateMap<Opportunity, OpportunityViewModel>().ForMember(
+            dest => dest.CampaignSourceName,
+            opt => opt.MapFrom(src => src.CampaignSource.Name)).ForMember(
+            dest => dest.CompanyOwner,
+            opt => opt.MapFrom(src => src.Company.Owner)).ForMember(
+            dest => dest.ContactOwner,
+            opt => opt.MapFrom(src => src.Contact.Owner)).ForMember(
+            dest => dest.LeadSourceName,
+            opt => opt.MapFrom(src => src.LeadSource.Name)).ReverseMap();
+            cfg.CreateMap<Project, ProjectViewModel>().ReverseMap();
+            cfg.CreateMap<Region, RegionViewModel>().ForMember(
+            dest => dest.CityName,
+            opt => opt.MapFrom(src => src.City.Name)).ReverseMap();
+            cfg.CreateMap<Report, ReportViewModel>().ReverseMap();
+            cfg.CreateMap<Sector, SectorViewModel>().ReverseMap();
+            cfg.CreateMap<WorkItem, WorkItemViewModel>().ForMember(
+            dest => dest.ProjectName,
+            opt => opt.MapFrom(src => src.Project.Name)).ReverseMap();
+            cfg.CreateMap<Document, DocumentViewModel>().ReverseMap();
+            cfg.CreateMap<LeadSource, LeadSourceViewModel>().ReverseMap();
+            cfg.CreateMap<LeadStatus, LeadStatusViewModel>().ReverseMap();
+            cfg.CreateMap<Country, CountryViewModel>().ReverseMap();
             Mapper.Initialize(cfg);
         }
     }
