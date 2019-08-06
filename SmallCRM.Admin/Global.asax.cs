bdb4b0e71174c18c944a54406fc7118c04c4a873
tugceguclu <tugceegucluu@gmail.com>
2019-08-06 13:46:56 +0300

    Project & CampaignSource ControllerUpdates

--- SmallCRM.Admin/Global.asax.cs
+++ SmallCRM.Admin/Global.asax.cs
@@ -27,6 +27,8 @@ namespace SmallCRM.Admin
             builder.Register(c => HttpContext.Current).InstancePerRequest();
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
             builder.RegisterType<ActivityService>().As<IActivityService>();
+            builder.RegisterType<ProjectService>().As<IProjectService>();
+            builder.RegisterType<CampaignSourceService>().As<ICampaignSourceService>();
 
             IContainer container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
