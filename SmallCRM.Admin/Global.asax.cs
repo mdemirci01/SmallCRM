74fc41e413ae9907c9a960dc489eb8c0e0f53fa9
Section1 <Section1@WISSEN>
2019-08-06 09:51:25 +0300

    automapper configüre edilebilir.

--- SmallCRM.Admin/Global.asax.cs
+++ SmallCRM.Admin/Global.asax.cs
@@ -1,6 +1,10 @@
+using Autofac;
+using Autofac.Integration.Mvc;
+using SmallCRM.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -13,6 +17,21 @@ namespace SmallCRM.Admin
     {
         protected void Application_Start()
         {
+           var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<ApplicationDbContext>().AsSelf();
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+            builder.Register(c => HttpContext.Current).InstancePerRequest();
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            //builder.RegisterType<CustomerService>().As<ICustomerService>();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            new AutoMapperConfig().Initialize();
+            
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
