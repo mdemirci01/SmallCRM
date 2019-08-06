136480ea15940b088753bb4d573c32df51fde534
dogucanozturk <dogucanozturk12@gmail.com>
2019-08-06 09:43:09 +0300

    05.08

--- SmallCRM.Admin/Models/LeadSourceViewModel.cs
+++ SmallCRM.Admin/Models/LeadSourceViewModel.cs
@@ -8,9 +8,12 @@ namespace SmallCRM.Admin.Models
 {
     public class LeadSourceViewModel
     {
+        public Guid Id { get; set; }
         [Required]
         [MaxLength(50)]
         [Display(Name="Müşteri Adayı Kaynağı")]
         public string Name { get; set; }
+        [Display(Name="Aktif mi?")]
+        public bool IsActive { get; set; }
     }
 }
