136e82b24fc888fa3102ed20fae4e162a4b0e59a
dogucanozturk <dogucanozturk12@gmail.com>
2019-08-06 14:07:16 +0300

    06.08(3) merge

--- SmallCRM.Admin/Controllers/LeadSourcesController.cs
+++ SmallCRM.Admin/Controllers/LeadSourcesController.cs
@@ -123,13 +123,6 @@ namespace SmallCRM.Admin.Controllers
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        
     }
 }
