4f4cc5d9ab35760debc9a8f71764a5f6745dfaee
ccrncck@gmail.com <ccrncck@gmail.com>
2019-08-07 11:21:55 +0300

    Global.asax eklendi

--- SmallCRM.Admin/Global.asax.cs
+++ SmallCRM.Admin/Global.asax.cs
@@ -29,7 +29,24 @@ namespace SmallCRM.Admin
             builder.Register(c => HttpContext.Current).InstancePerRequest();
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
             builder.RegisterType<ActivityService>().As<IActivityService>();
+            builder.RegisterType<CampaignService>().As<ICampaignService>();
+            builder.RegisterType<CampaignSourceService>().As<ICampaignSourceService>();
+            builder.RegisterType<CityService>().As<ICityService>();
+            builder.RegisterType<CompanyService>().As<ICompanyService>();
+            builder.RegisterType<ContactService>().As<IContactService>();
+            builder.RegisterType<CountryService>().As<ICountryService>();
+            builder.RegisterType<DocumentService>().As<IDocumentService>();
+            builder.RegisterType<FeedService>().As<IFeedService>();
+            builder.RegisterType<LeadService>().As<ILeadService>();
             builder.RegisterType<LeadSourceService>().As<ILeadSourceService>();
+            builder.RegisterType<LeadStatusService>().As<ILeadStatusService>();
+            builder.RegisterType<OpportunityService>().As<IOpportunityService>();
+            builder.RegisterType<ProjectService>().As<IProjectService>();
+            builder.RegisterType<RegionService>().As<IRegionService>();
+            builder.RegisterType<ReportService>().As<IReportService>();
+            builder.RegisterType<SectorService>().As<ISectorService>();
+            builder.RegisterType<TimeSpendingService>().As<ITimeSpendingService>();
+            builder.RegisterType<WorkItemService>().As<IWorkItemService>();
 
             IContainer container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
