4869010a09abe8c3f19a72360eea96852352ee46
BusraOzdemir <busra.ozdemir@ceng.ktu.edu.tr>
2019-08-07 11:55:42 +0300

    LeadStatus düzenlemeler

---
+++ SmallCRM.Admin/Controllers/LeadStatusController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using SmallCRM.Admin.Models;
+using SmallCRM.Data;
+using SmallCRM.Model;
+using SmallCRM.Service;
+
+namespace SmallCRM.Admin.Controllers
+{
+    public class LeadStatusController : Controller
+    {
+        private readonly ILeadStatusService leadStatusService;
+
+        // GET: LeadStatus
+        public LeadStatusController(ILeadStatusService leadStatusService)
+        {
+            this.leadStatusService = leadStatusService;
+        }
+        public ActionResult Index()
+        {
+            var leadStatues = Mapper.Map<IEnumerable<LeadStatusViewModel>>(leadStatusService.GetAll());
+            return View(leadStatues);
+        }
+
+        // GET: LeadStatus/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LeadStatusViewModel leadStatus = Mapper.Map<LeadStatusViewModel>(leadStatusService.Get(id.Value));
+            if (leadStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leadStatus);
+        }
+
+        // GET: LeadStatus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeadStatus/Create
+        // Aşırı gönderim saldırılarından korunmak için, lütfen bağlamak istediğiniz belirli özellikleri etkinleştirin, 
+        // daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=317598 sayfasına bakın.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create( LeadStatusViewModel leadStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = Mapper.Map<LeadStatus>(leadStatus);
+                leadStatusService.Insert(entity);
+                return RedirectToAction("Index");
+            }
+
+            return View(leadStatus);
+        }
+
+        // GET: LeadStatus/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LeadStatusViewModel leadStatus = Mapper.Map<LeadStatusViewModel>(leadStatusService.Get(id.Value));
+            if (leadStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leadStatus);
+        }
+
+        // POST: LeadStatus/Edit/5
+        // Aşırı gönderim saldırılarından korunmak için, lütfen bağlamak istediğiniz belirli özellikleri etkinleştirin, 
+        // daha fazla bilgi için https://go.microsoft.com/fwlink/?LinkId=317598 sayfasına bakın.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(LeadStatusViewModel leadStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = Mapper.Map<LeadStatus>(leadStatus);
+                leadStatusService.Update(entity);
+                return RedirectToAction("Index");
+            }
+            return View(leadStatus);
+        }
+
+        // GET: LeadStatus/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LeadStatusViewModel leadStatus = Mapper.Map<LeadStatusViewModel>(leadStatusService.Get(id.Value));
+            if (leadStatus == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leadStatus);
+        }
+
+        // POST: LeadStatus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            leadStatusService.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
--- SmallCRM.Admin/Models/LeadStatusViewModel.cs
+++ SmallCRM.Admin/Models/LeadStatusViewModel.cs
@@ -8,9 +8,13 @@ namespace SmallCRM.Admin.Models
 {
     public class LeadStatusViewModel
     {
+        public Guid Id { get; set; }
         [Required]
         [MaxLength(50)]
         [Display(Name="Müşteri Adayı Durumu")]
         public string Name { get; set; }
+
+        [Display(Name = "Aktif Mi?")]
+        public bool IsActive { get; set; }
     }
 }
