9253c4434daae49999369c5f8d9efb4de33342a8
cemre <cemre630@hotmail.com>
2019-08-08 11:07:35 +0300

    blabla

--- SmallCRM.Model/TitleOfCourtesy.cs
+++ SmallCRM.Model/TitleOfCourtesy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,21 @@ namespace SmallCRM.Model
 {
     public enum TitleOfCourtesy
     {
+        [Display(Name = "Yok")]
         None = 0,
+        [Display(Name = "Erkek")]
         Mr = 1,
+        [Display(Name = "Kadın")]
         Mrs = 2,
+        [Display(Name = "Doktor")]
         Dr = 3,
+        [Display(Name = "Profesör")]
         Prof = 4,
+        [Display(Name = "Doçent")]
         AssociateProf = 5,
+        [Display(Name = "Yardımcı Doçent")]
         AssistantProf = 6,
+        [Display(Name = "Avukat")]
         Lawyer = 7
     }
 }
