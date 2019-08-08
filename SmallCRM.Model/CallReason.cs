c2deeeca12ca70290c693cde4ed93ec7f66f3cea
ccrncck@gmail.com <ccrncck@gmail.com>
2019-08-08 11:05:59 +0300

    Activity'nin Enum'larına DataAnnotation eklendi.

--- SmallCRM.Model/CallReason.cs
+++ SmallCRM.Model/CallReason.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,19 @@ namespace SmallCRM.Model
 {
     public enum CallReason
     {
+        [Display(Name = "Bulunamadı")]
         NotFound = 0,
+        [Display(Name = "İnceleniyor")]
         Inspecting = 1,
+        [Display(Name = "Yönetimsel")]
         Administrative = 2,
+        [Display(Name = "Müzakere")]
         Negotiation = 3,
+        [Display(Name = "Demo")]
         Demo = 4,
+        [Display(Name = "Proje")]
         Project = 5,
+        [Display(Name = "Destek")]
         Support = 6
 
     }
