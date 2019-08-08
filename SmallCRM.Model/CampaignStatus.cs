b0a7aad95d4493da33616e73b7e8111dc1e85f5d
BusraOzdemir <busra.ozdemir@ceng.ktu.edu.tr>
2019-08-08 11:04:58 +0300

    Enum display name

--- SmallCRM.Model/CampaignStatus.cs
+++ SmallCRM.Model/CampaignStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace SmallCRM.Model
 {
     public enum CampaignStatus
     {
-        None=0,
+        [Display(Name = "-Yok-")]
+        None =0,
+        [Display(Name = "Planlanıyor")]
         Planning = 1,
+        [Display(Name = "Aktif")]
         Active = 2,
+        [Display(Name = "Aktif Değil")]
         NonActive = 3,
+        [Display(Name = "Tamamlandı")]
         Completed = 4
     }
    
