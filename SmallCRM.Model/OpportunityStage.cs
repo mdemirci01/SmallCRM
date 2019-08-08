03633db85a250ac259938cb42dcfd0ab06b9c68d
koseirem1 <koseirem1@gmail.com>
2019-08-08 10:56:35 +0300

    Opportunity enum lar düzeltildi

--- SmallCRM.Model/OpportunityStage.cs
+++ SmallCRM.Model/OpportunityStage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,24 @@ namespace SmallCRM.Model
 {
     public enum OpportunityStage
     {
+        [Display(Name ="Yok")]
         None=0,
+        [Display(Name = "Görüşülüyor/Değerlendiriliyor")]
         Evaluation = 1,
-        AnalysisRequired=2,
-        SuggestingValue=3,
-        IdentifyMadeDecisions=4,
-        OfferingPrice=5,
-        ClosedWon=6,
-        ClosedLost=7,
-        LostToCompetition=8
+        [Display(Name = "Analiz Gerekiyor")]
+        AnalysisRequired =2,
+        [Display(Name = "Değer Öneriliyor")]
+        SuggestingValue =3,
+        [Display(Name = "Karar Verenleri Tanımla")]
+        IdentifyMadeDecisions =4,
+        [Display(Name = "Fiyat Teklifi")]
+        OfferingPrice =5,
+        [Display(Name = "Kapandı-Kazanıldı")]
+        ClosedWon =6,
+        [Display(Name = "Kapandı-Kaybedildi")]
+        ClosedLost =7,
+        [Display(Name = "Rekabette Kapandı-Kaybedildi")]
+        LostToCompetition =8
 
 
 
